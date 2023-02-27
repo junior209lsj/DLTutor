484725de8bccca93b1386f2d5f0f4db532e5a2b8
seongjae <seongjae.lee.1118@gmail.com>
2023-02-27 22:53:54 +0900

    feat: add example test code

--- test/test1_tensor.cc
+++ test/test1_tensor.cc
@@ -1,5 +1,13 @@
-#include <src/tensor.h>
+#include <iostream>
+#include <tensor.h>
 
 int main(void) {
+
+    dltu::Tensor<float> t({10, 10});
+
+    std::cout << t << std::endl;
+
+    std::cout << t[0] << std::endl;
+
     return 0;
 }
