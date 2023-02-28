bf5a4337f32ecf881ccb420a2b83239dd5c24ccc
seongjae <seongjae.lee.1118@gmail.com>
2023-03-01 01:13:13 +0900

    test: test 코드 수정

--- test/test1_tensor.cc
+++ test/test1_tensor.cc
@@ -3,10 +3,10 @@
 
 int main(void) {
 
-    dltu::Tensor<float> t({10, 10});
+    dltu::Tensor<float> t({2, 3}, {1, 2, 3, 4, 5, 6});
 
-    for(size_t i = 0; i < 10; i++) {
-        for(size_t j = 0; j < 10; j++) {
+    for(size_t i = 0; i < 2; i++) {
+        for(size_t j = 0; j < 3; j++) {
             std::cout << t({i, j}) << ", ";
         }
         std::cout << std::endl;
