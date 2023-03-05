833f5fcbcf830159eafa5b58633545e68ef5e32f
seongjae <seongjae.lee.1118@gmail.com>
2023-03-06 01:26:49 +0900

    test: add testing code (compute index)

--- test/test1_tensor.cc
+++ test/test1_tensor.cc
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <tensor.hpp>
 
-int main(void) {
+#include <gtest/gtest.h>
 
-    dltu::Tensor<float> t({2, 3}, {1, 2, 3, 4, 5, 6});
+TEST(Tensor, index_computation) {
+  dltu::Tensor<float> t({2, 2, 2}, {1, 2, 3, 4, 5, 6, 7, 8});
+  EXPECT_EQ(1, t({0, 0, 0}));
+  EXPECT_EQ(2, t({0, 0, 1}));
+  EXPECT_EQ(3, t({0, 1, 0}));
+  EXPECT_EQ(4, t({0, 1, 1}));
+  EXPECT_EQ(5, t({1, 0, 0}));
+  EXPECT_EQ(6, t({1, 0, 1}));
+  EXPECT_EQ(7, t({1, 1, 0}));
+  EXPECT_EQ(8, t({1, 1, 1}));
+}
 
-    for(size_t i = 0; i < 2; i++) {
-        for(size_t j = 0; j < 3; j++) {
-            std::cout << t({i, j}) << ", ";
-        }
-        std::cout << std::endl;
-    }
-
-    return 0;
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
